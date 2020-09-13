ccba57002099c9c81343ab86199ab70942a138b9
unknown <alejandrojzeballos@gmail.com>
2020-09-13 01:14:04 -0400

    hello world en php

---
+++ apuntes/apuntes.php
@@ -0,0 +1,26 @@
+<?
+/* apuntes curso */
+
+/* 
+   php
+    php: hypertext pre_processor
+    php es un lenguaje descriptivo no un programing languaje
+    la diferenecia es que esperan acciones, por ejemplo una pagina web espera a la accion del boton.
+    puedes extender la funcionalidad de tus paginas web.
+    ventajas
+    open source
+    cross platform
+    powerful, robust and scalable 
+    si tienes bastante trafico php es lo que buscas
+
+    progrmas
+    web server - wamp for windows
+    php 7
+    database
+    text editor 
+    web browser
+
+    lamp - linux
+    mamp-  mac
+*/
+?>
