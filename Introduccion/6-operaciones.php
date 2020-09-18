7f7c8fc2e301c71857bcb18bcb9a96249c9ee05e
unknown <alejandrojzeballos@gmail.com>
2020-09-18 19:30:54 -0400

    operacoines y funcines de numeros

---
+++ Introduccion/6-operaciones.php
@@ -0,0 +1,19 @@
+<?php
+    //operaciones con php
+    echo pi();  //imprime el valor de pi
+
+    //minimo
+    echo(min(0,150,20,30,-8,-200)); //devuelve -200
+    //MAXIMO
+    echo(max(0,150,20,30,-8,-200)); //devuelve 150
+
+    echo(abs(-6.7));    //retorna el valor absoluto
+
+    echo(sqrt(64));     //retorna la raiz cuadrada
+
+    echo(round(0.60));  //redondea 1
+    echo(round(0.49));  // redondea a 0
+
+    echo(rand());       //saca un random
+    echo(rand(10,100)); //saca un random entre 10 y 100
+?>
