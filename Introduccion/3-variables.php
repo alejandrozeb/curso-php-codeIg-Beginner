0dfd722d8a08e06dd7f041354d27a13aa2192fb1
unknown <alejandrojzeballos@gmail.com>
2020-09-17 23:31:03 -0400

    variables

---
+++ Introduccion/3-variables.php
@@ -0,0 +1,13 @@
+<?php
+    $num1=75;
+    $num2 = 25;
+    $result = $num1 + $num2;
+    echo $result;
+    echo $num1+$num2;
+?>
+<br>
+<?php
+    $text1= 'Hello';
+    $text2 = 'World';
+    echo $text1." ",$text2;
+?>
