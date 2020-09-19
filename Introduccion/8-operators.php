6311da0aefa8e8cdc3d827dbfc11165567b10532
unknown <alejandrojzeballos@gmail.com>
2020-09-18 20:36:04 -0400

    Operadores en php

---
+++ Introduccion/8-operators.php
@@ -0,0 +1,41 @@
+<?php
+    //php asigment Operators
+    //asignaciones
+  /*   x=y
+    x+=y    //x=x+y
+    x-=y    //x=x-y
+    x*=y    //x=x*y
+    x/=y    //x=x/y division
+    x%=y    //x=x%y modulo
+ */
+
+ //Comparison operators
+
+   /*  == //equal
+    === //identical tomando en cuenta el case sensitive, y el tipo de variable
+    != //no es igual
+    !== //no identico
+    >   //mayor
+    <   //menor
+    >= //Gmayor igual
+    <= //menor igual
+
+    //incrementos 
+
+    ++$x //pre incremento
+    $x++    //postincremetno
+    --$x    //pre decremento
+    $x--    //post decremento
+
+    //operadores logicos
+
+    and And $X and $y
+    or Or   $X or $y    //uno de ellos o ambos pueden ser true
+    xor xor $X xor $y   //uno de ellos debe ser true, nunca los dos
+    && And  $X && $y
+    || Or   $X || $y
+    ! not   !$x
+    //ternario if corto
+
+    ? ternary   $x = expr1 ? expr2 : expr3 */
+?>
