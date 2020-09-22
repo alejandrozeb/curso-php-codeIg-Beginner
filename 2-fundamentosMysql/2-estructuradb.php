cd04b0a494b9b02a9a76387d59a81239a3e47484
unknown <alejandrojzeballos@gmail.com>
2020-09-22 01:18:04 -0400

    añadiendo una explicacion breve de una bd

---
+++ 2-fundamentosMysql/2-estructuradb.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+    puedes imaginar una caja con elementos relacionados que seran nuestra data donde podemos encontrar elementos los cuales van a tener atributos una pelota por ejemplo tenfra atribtos como tamaño peso y tambien un id, un id es un identificador unico para ese elemento, esa seria una BD
+
+    table
+        row
+         colum colum colum
+
+    esta es la estructura normal de una tabla en una bd donde table puede ser la pelota row un atibuto y la columna el dato
+
+    puedes revisar mas buscando Basededatos relacional y base de datos no relacional que son las que mas se usan en desarrollo web
+*/
+?>
