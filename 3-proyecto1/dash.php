8d0d43c0a3e8a335cf79f86887ec06a08b36aaf2
unknown <alejandrojzeballos@gmail.com>
2020-09-25 20:28:14 -0400

    añadiendo proyecto 1 con php nativo

---
+++ 3-proyecto1/dash.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>Welcome</h1>
+    
+    <!-- main content -->
+
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ 3-proyecto1/register.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <!-- register -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-lg-push-4 col-md-push-4">
+                <div class="panel panel-default" style="margin-top: 50px;">
+                    <div class="panel-heading">Login</div>
+                    <div class="panel-body">
+                        <form action="" method="POST">
+                            <div class="form-group">
+                                <input type="email" class="form-control input-sm" name="u_email" required placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control input-sm" name="u_name" required placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control input-sm" name="u_pass" required placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-success btn-sm" name="u_reg" value="Register">
+                                <a href="index.php" class="btn btn-info btn-sm">Login</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- register -->
+
+  
+
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
