134c5bece95c58c574cd145568431910e55be503
unknown <alejandrojzeballos@gmail.com>
2020-09-28 22:04:24 -0400

    añadiendo nuevos empleados

--- 3-proyecto1/add_new_employes.php
+++ 3-proyecto1/add_new_employes.php
@@ -44,42 +44,47 @@
           </div>
           <div class="col-lg-9 col-md-9">
             <div class="panel panel-default">
-                <div class="panel-heading">Employess List</div>
-                <table class="table table-bordered">
-                    <tr>
-                        <td>ID</td>
-                        <td>Name</td>
-                        <td>Details</td>
-                        <td>Edit</td>
-                        <td>Deleted</td>
-                    </tr>
-                    <?php
-                     $sql="SELECT * FROM employes";
-                     $result= mysqli_query($conn,$sql);
-         
-                     if(mysqli_num_rows($result)>0){
-                         while ($employe = mysqli_fetch_assoc($result)) //de result saca un row a user
-                         {  ?>
-
-                          <tr>
-                            <td><?php echo $employe['e_id'];?></td>
-                            <td><?php echo $employe['e_name'];?></td>
-                            <td><a href="#" class="btn btn-xs btn-info btn-block">Details</a></td>
-                            <td><a href="#" class="btn btn-xs btn-warning btn-block">Edit</a></td>
-                            <td><a href="#" class="btn btn-xs btn-Danger btn-block">Deleted</a></td>
-                          </tr>
-
-                        <?php }
-                     }else{
-                       echo '0 results';
-                     }
-                    ?>
-                </table>  
+                <div class="panel-heading">add Employe</div>
+                <div class="panel-body">
+                    <form action="" method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control input-sm" name="e_name" placeholder="Employee name" require>
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control input-sm" name="e_email" placeholder="Employee email" require>
+                        </div>
+                        <div class="form-group">
+                            <input type="tel" class="form-control input-sm" name="e_phone" placeholder="Employee phone number" require>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-sm btn-success" value="Add_Employee" name='e_add'>
+                        </div>
+                    </form>
+                </div>
             </div>
           </div>
         </div>
       </div>
     <!-- main content -->
+    <?php
+        $sql="";
+
+        if(isset($_POST['e_add'])){
+        $e_name = $_POST['e_name'];
+        $e_email = $_POST['e_email'];
+        $e_phone = $_POST['e_phone'];
+        //creando la consulta
+        $sql = "INSERT INTO employes(e_name,e_email,e_phone) VALUES ('$e_name','$e_email','$e_phone')";
+
+        //realizando la consulta
+        if(mysqli_query($conn,$sql)){
+            echo "<script>alert('Nuevo registro creado')</script>";
+        }else{
+            echo "error $sql <br>".mysqli_error($conn);
+        }
+
+        }
+    ?>
 
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
