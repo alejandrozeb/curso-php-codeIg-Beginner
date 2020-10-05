3a3c8b39067c713591434519b02dcf3ed22ab76f
unknown <alejandrojzeballos@gmail.com>
2020-10-04 22:27:37 -0400

    añadiendo el header y el footer en distintos archivos

--- 4-ci/application/controllers/Home.php
+++ 4-ci/application/controllers/Home.php
@@ -6,8 +6,12 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		//cargamos el header
+		$this->load->view('inc/header');//podemos navegar a uestro archivo	
         //vamos a crear la pagina por defecto de nuestro proyecto
-        $this->load->view('home');
+		$this->load->view('home');
+		//cargamos el footer
+		$this->load->view('inc/footer');
 	}
 
 }
