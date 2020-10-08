d46bdacf0bc121d4754a46d1a8923d2ce5dc581b
unknown <alejandrojzeballos@gmail.com>
2020-10-08 19:31:33 -0400

    añdiendo el form para crear un job

--- 4-ci/application/views/dash/inc/sidebar.php
+++ 4-ci/application/views/dash/inc/sidebar.php
@@ -4,12 +4,11 @@
                 <a href="#" class="list-group-item">Add Employee</a>
                 <a href="#" class="list-group-item">Employees List</a>
             </div>
-        </div>
-        <div class="panel panel-default">
+    </div>
+    <div class="panel panel-default">
             <div class="panel-heading">Jobs Action</div>
             <div class="list-group">
-                <a href="#" class="list-group-item">Add Jobs</a>
+                <a href="<?php echo site_url();?>jobs" class="list-group-item">Add Job</a>
                 <a href="#" class="list-group-item">Jobs List</a>
             </div>
-        </div>
     </div>
