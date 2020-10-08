809b86d3c9e094f2312e94ae1e0c3ae87d03ebe7
unknown <alejandrojzeballos@gmail.com>
2020-10-08 02:55:42 -0400

    añadiendo el nav

--- 4-ci/application/views/dash/dash_home.php
+++ 4-ci/application/views/dash/dash_home.php
@@ -4,5 +4,22 @@ if(!$_SESSION['u_name']){
     redirect('home','refresh');
 }
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Proyecto PHP</title>
 
-Welcome <?php echo $_SESSION['u_name']; ?>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href= "<?php echo base_url();?>assets/css/bootstrap.min.css">
+  </head>
+  <body>
+    <!-- dashnav -->
+    <?php  $this->load->view('dash/inc/nav'); ?>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js" ></script>
+  </body>
+</html>
